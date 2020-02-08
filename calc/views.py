a6a328e9c399b35d57db98455ce24289f4126614
Franklin okech <franklinokech@gmail.com>
2020-02-08 14:04:07 +0300

    Started the real learning

--- calc/views.py
+++ calc/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Hello world Franklin')
+    return render(request, 'home.html', {'name': 'Franklin', 'age': 23})
